2a8b81f7ce7078f71913436b592d7d5c3102bcf8
beckho <vngkgk624@gmail.com>
2020-01-16 22:21:37 +0900

    이슈 큐브 관리자 개발

--- src/View/CubePage/IssuecubePage.vue
+++ src/View/CubePage/IssuecubePage.vue
@@ -19,14 +19,83 @@
             ></v-text-field>
             <v-btn color="primary" class="ml-4" @click="onsearchclick">검색</v-btn>
           </v-row>
+          <v-row>
+            <v-select
+              style="max-width:50px"
+              v-model="options.itemsPerPage"
+              :items="itemperpagelist"
+            ></v-select>
+            <v-spacer></v-spacer>
+            <v-dialog persistent v-model="deletecheckdialog" width="500">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" v-on="on">큐브 삭제</v-btn>
+              </template>
+              <v-card>
+                <v-card-text>
+                  <v-col>
+                    <v-row class="headline">정말로 삭제 하시겠습니까?</v-row>
+                    <v-row justify="end">
+                      <v-btn color="primary" @click="deletecheckdialog=false">아니요</v-btn>
+                      <v-btn color="primary" class="ml-4" @click="ondeletecube">삭제</v-btn>
+                    </v-row>
+                  </v-col>
+                </v-card-text>
+              </v-card>
+            </v-dialog>
+          </v-row>
+          <v-row>
+            <v-col cols="12" class="ma-0 pa-0">
+              <v-card>
+                <v-data-table
+                  v-model="selected"
+                  show-select
+                  item-key="cubeuuid"
+                  :options.sync="options"
+                  :items="items"
+                  :loading="isloading"
+                  :headers="header"
+                  hide-default-footer
+                >
+                  <template v-slot:item.activationTime="{ item }">
+                    {{
+                    Math.trunc(item.activationTime.diffNow("hour").hours)
+                    }}
+                  </template>
+                  <template
+                    v-slot:item.makeTime="{ item }"
+                  >{{item.makeTime.toFormat("yyyy-MM-dd HH:mm:ss")}}</template>
+                </v-data-table>
+                <v-pagination
+                  @input="onchangepage"
+                  v-model="pagenationpage"
+                  :length="totalpagelength"
+                ></v-pagination>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-col>
+    <v-dialog v-model="deletecheckdialog2" width="500">
+      <v-card>
+        <v-card-text>
+          <v-col>
+            <v-row class="headline">삭제하였습니다.</v-row>
+            <v-row justify="end">
+              <v-btn color="primary" @click="deletecheckdialog2 = false">확인</v-btn>
+            </v-row>
+          </v-col>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import Fcubequestpageview from "@/components/CubePage/Fcubequestpageview";
+import FcubequestpageviewSearchdto from "@/components/CubePage/FcubequestpageviewSearchdto";
+import { DateTime } from "luxon";
 interface TableHeader {
   text: string;
   value: string;
@@ -89,11 +158,107 @@ export default class IssuecubePage extends Vue {
     multiSort: false,
     mustSort: false
   };
+  header: TableHeader[] = [
+    {
+      text: "제목",
+      value: "cubeName"
+    },
+    {
+      text: "제작자",
+      value: "nickName"
+    },
+    {
+      text: "위치",
+      value: "placeAddress"
+    },
+    {
+      text: "View",
+      value: "cubeHits",
+      sortable: true
+    },
+    {
+      text: "좋아요",
+      value: "cubeLikes",
+      sortable: true
+    },
+    {
+      text: "싫어요",
+      value: "cubeDisLikes",
+      sortable: true
+    },
+    {
+      text: "참가자",
+      value: "joinPlayer",
+      sortable: true
+    },
+    {
+      text: "보상P",
+      value: "makeExp"
+    },
+    {
+      text: "남은 생존 시간(시)",
+      value: "activationTime",
+      sortable: true
+    },
+    {
+      text: "등록일",
+      value: "makeTime",
+      sortable: true
+    }
+  ];
   searchitem = "cubeName";
   searchtext = "";
   totalpagelength = 0;
   pagenationpage = 1;
-  onsearchclick() {}
+  deletecheckdialog = false;
+  selected: Fcubequestpageview[] = [];
+  items: Fcubequestpageview[] = [];
+  deletecheckdialog2 = false;
+  isloading = false;
+  onsearchclick() {
+    this.onsearch(this.options);
+  }
+  async ondeletecube() {
+    for (let i = 0; i < this.selected.length; i++) {
+      await this.selected[i].deletefcubeitem();
+    }
+    this.onsearch(this.options);
+    this.deletecheckdialog = false;
+    this.deletecheckdialog2 = true;
+  }
+  onchangepage(value: number) {
+    this.options.page = value;
+  }
+  @Watch("options", { immediate: true, deep: true })
+  async onoptions(value: DataOptions) {
+    this.onsearch(value);
+  }
+  async onsearch(value: DataOptions) {
+    this.isloading = true;
+    let fcubeUserSearchdto: FcubequestpageviewSearchdto = {
+      offset: (value.page - 1) * value.itemsPerPage,
+      limit: value.itemsPerPage,
+      sortBy: value.sortBy[0],
+      sortDesc: value.sortDesc[0],
+      searchtype: this.searchitem,
+      searchtext: this.searchtext,
+      cubeType: "issuecube"
+    };
+    this.options.page = value.page;
+    let temptotalpagelength: number = await Fcubequestpageview.getFcubeQuestPageViewlength(
+      fcubeUserSearchdto
+    );
+    this.isloading = false;
+    this.totalpagelength =
+      Math.trunc(temptotalpagelength / value.itemsPerPage) + 1;
+    this.items = await Fcubequestpageview.getFcubequestpageviews(
+      fcubeUserSearchdto
+    );
+
+    
+
+    this.isloading = false;
+  }
 }
 </script>
 
